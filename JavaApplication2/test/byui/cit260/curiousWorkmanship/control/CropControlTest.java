1327821b7832dd509ddab7585b18cbbb71edc9da
nimax <nimax29@gmail.com>
2018-02-13 01:10:22 -0500

    2/3/2018 - Group Assignment 6

--- JavaApplication2/test/byui/cit260/curiousWorkmanship/control/CropControlTest.java
+++ JavaApplication2/test/byui/cit260/curiousWorkmanship/control/CropControlTest.java
@@ -46,11 +46,11 @@ public class CropControlTest {
         int landPrice =20;
         int expResult =2810;
         int result = CropControl.buyland(acresToBuy, landPrice, cropyData);
-        assertEquals(expResult, result);
+        assertEquals(expResult,result);
         System.out.println(result);
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-        
+        fail("The test case is a prototype."); 
+       
         
     }
     
