f202b33a74941dd2cfaccc5c781c2f5f5ddb254f
Tejada-Joyce <35240350+Tejada-Joyce@users.noreply.github.com>
2018-02-15 21:44:09 -0500

    5 Test Cases

--- JavaApplication2/test/byui/cit260/curiousWorkmanship/control/CropControlTest.java
+++ JavaApplication2/test/byui/cit260/curiousWorkmanship/control/CropControlTest.java
@@ -37,24 +37,109 @@ public class CropControlTest {
     @Test
     public void testBuyland() {
         System.out.println("buyland");
+        
+        //-----Test Case 1 ------
+        System.out.println("\tTest case 1");
         //define some variables of the game
         CropData cropyData = new CropData(); // constructor
         cropyData.setWheatInStore(1000);
-        cropyData.setAcresOwned(2000);
-        cropyData.setPopulation(10);
+        cropyData.setAcresOwned(2800);
+        cropyData.setPopulation(4);
         
-        int acresToBuy =100;
+        int acresToBuy =10;
         //random function
-        int landPrice =10;
+        int landPrice =20;
         //value we expected
-        int expResult =2100;
+        int expResult =2810;
         //call the method and assign the value into a variable
         int result = CropControl.buyland(acresToBuy, landPrice, cropyData);
         //a camparation is made to know if the result is the expected
-        assertEquals(expResult,result);
+        assertEquals(expResult,result,2810);
         //print the result
         System.out.println(result);
-        // TODO review the generated test code and remove the default call to fail.
+        
+        //-----Test Case 2 ------
+        System.out.println("\tTest case 2");
+        //define some variables of the game
+        CropData cropyData2 = new CropData(); // constructor
+        cropyData2.setWheatInStore(1000);
+        cropyData2.setAcresOwned(2800);
+        cropyData2.setPopulation(4);
+        
+        acresToBuy =-5;
+        //random function
+        landPrice =20;
+        //value we expected
+        expResult =-1;
+        //call the method and assign the value into a variable
+        result = CropControl.buyland(acresToBuy, landPrice, cropyData2);
+        //a camparation is made to know if the result is the expected
+        assertEquals(expResult,result,-1);
+        //print the result
+        System.out.println(result);
+        
+        
+        //-----Test Case 3 ------
+        System.out.println("\tTest case 3");
+        //define some variables of the game
+        CropData cropyData3 = new CropData(); // constructor
+        cropyData3.setWheatInStore(1000);
+        cropyData3.setAcresOwned(2800);
+        cropyData3.setPopulation(6);
+        
+        acresToBuy =3000;
+        //random function
+        landPrice =20;
+        //value we expected
+        expResult =-1;
+        //call the method and assign the value into a variable
+        result = CropControl.buyland(acresToBuy, landPrice, cropyData3);
+        //a camparation is made to know if the result is the expected
+        assertEquals(expResult,result,-1);
+        //print the result
+        System.out.println(result);
+        
+        
+        //-----Test Case 4 ------
+        System.out.println("\tTest case 4");
+        //define some variables of the game
+        CropData cropyData4 = new CropData(); // constructor
+        cropyData4.setWheatInStore(1000);
+        cropyData4.setAcresOwned(2000);
+        cropyData4.setPopulation(10);
+        
+        acresToBuy =100;
+        //random function
+        landPrice =10;
+        //value we expected
+        expResult =2100;
+        //call the method and assign the value into a variable
+        result = CropControl.buyland(acresToBuy, landPrice, cropyData4);
+        //a camparation is made to know if the result is the expected
+        assertEquals(expResult,result,2100);
+        //print the result
+        System.out.println(result);
+        
+        
+        //-----Test Case 5 ------
+        System.out.println("\tTest case 5");
+        //define some variables of the game
+        CropData cropyData5 = new CropData(); // constructor
+        cropyData5.setWheatInStore(1000);
+        cropyData5.setAcresOwned(2000);
+        cropyData5.setPopulation(10);
+        
+        acresToBuy =0;
+        //random function
+        landPrice =10;
+        //value we expected
+        expResult =2000;
+        //call the method and assign the value into a variable
+        result = CropControl.buyland(acresToBuy, landPrice, cropyData5);
+        //a camparation is made to know if the result is the expected
+        assertEquals(expResult,result,2000);
+        //print the result
+        System.out.println(result);
         
     }
     
