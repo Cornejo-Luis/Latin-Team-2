adb4666ee7f5c6e207512e3886b08a474b866ba0
Tejada-Joyce <35240350+Tejada-Joyce@users.noreply.github.com>
2018-03-10 22:06:23 -0500

    GameControl class - stubs

--- JavaApplication2/src/byui/cit260/curiousWorkmanship/control/GameControl.java
+++ JavaApplication2/src/byui/cit260/curiousWorkmanship/control/GameControl.java
@@ -4,11 +4,50 @@
  * and open the template in the editor.
  */
 package byui.cit260.curiousWorkmanship.control;
-
+import byui.cit260.curiousWorkmanship.model.Game;
 /**
  *
  * @author nimrod
  */
 public class GameControl {
     
+    public static int createNewGame (String theName) {
+        System.out.println("\ncreateNewGame method will be created here");
+        return 0;
+    }
+    
+    public static int createCropDataObject ( ) {
+        System.out.println("\ncreateCropDataObject method will be created here");
+        return 0;
+    }
+    
+    public static int createAnimalList ( ) {
+        System.out.println("\ncreateAnimalList method will be created here");
+        return 0;
+    }
+    
+    public static int createToolList ( ) {
+        System.out.println("\ncreateToolList method will be created here");
+        return 0;
+    }
+    
+    public static int createProvisionsList ( ) {
+        System.out.println("\ncreateProvisionsList method will be created here");
+        return 0;
+    }
+    
+    public static int createMap ( ) {
+        System.out.println("\ncreateMap method will be created here");
+        return 0;
+    }
+    
+    public static int saveGame (Game theGame, String theName ) {
+        System.out.println("\ncreateAnimalList method will be created here");
+        return 0;
+    }
+    
+    public static int getSavedGame (String theName ) {
+        System.out.println("\ngetSavedGame method will be created here");
+        return 0;
+    }
 }
